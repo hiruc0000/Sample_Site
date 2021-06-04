e52f85cf38ec06616b5478f8cdaff3e43018fd55
hiruc0000 <81366454+hiruc0000@users.noreply.github.com>
2021-06-04 15:10:01 +0900

    修正

--- js/openLinks.js
+++ js/openLinks.js
@@ -1,13 +1,33 @@
 (() => {
   const links = document.querySelectorAll('a[href]');
 
-  if (/(iphone)/i.test(navigator.userAgent) || /(ipad)/i.test(navigator.userAgent)) {
-    links.forEach((link) => {
-      link.href = link.href.replace('https://apps.apple.com/jp/app/', 'itms-apps://itunes.apple.com/app/');
-    });
-  } else if (/(android)/i.test(navigator.userAgent)) {
-    links.forEach((link) => {
-      link.href = link.href.replace('https://play.google.com/store/apps/', 'market://');
-    });
+  for (let i = 0; i < links.length; i++) {
+    const linkAgent = links[i].getAttribute('data-agent');
+
+    if (linkAgent === null) {
+      return;
+    }
+
+    if (linkAgent === 'ios') {
+      if (/(iphone)/i.test(navigator.userAgent) || /(ipad)/i.test(navigator.userAgent)) {
+        links.forEach((link) => {
+          link.href = link.href.replace('https://apps.apple.com/jp/app/', 'itms-apps://itunes.apple.com/app/');
+        });
+      }
+    }
   }
+
+  for (let i = 0; i < links.length; i++) {
+    const linkAgent = links[i].getAttribute('data-agent');
+
+    if (linkAgent === 'android') {
+      if (/(android)/i.test(navigator.userAgent)) {
+        links.forEach((link) => {
+          link.href = link.href.replace('https://play.google.com/store/apps/', 'market://');
+        });
+      }
+    }
+  }
+
+
 })();
