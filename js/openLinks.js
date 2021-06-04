35322bbda380de1f2c425d28e205bb1ac996e59a
hiruc0000 <81366454+hiruc0000@users.noreply.github.com>
2021-06-04 16:05:13 +0900

    修正2

--- js/openLinks.js
+++ js/openLinks.js
@@ -6,17 +6,13 @@
 
     if (linkAgent === null) {
       return;
-    }
-
-    if (linkAgent === 'ios') {
+    } else if (linkAgent === 'ios') {
       if (/(iphone)/i.test(navigator.userAgent) || /(ipad)/i.test(navigator.userAgent)) {
         links.forEach((link) => {
           link.href = link.href.replace('https://apps.apple.com/jp/app/', 'itms-apps://itunes.apple.com/app/');
         });
       }
-    }
-
-    if (linkAgent === 'android') {
+    } else if (linkAgent === 'android') {
       if (/(android)/i.test(navigator.userAgent)) {
         links.forEach((link) => {
           link.href = link.href.replace('https://play.google.com/store/apps/', 'market://');
